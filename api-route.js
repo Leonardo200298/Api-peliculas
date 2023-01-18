6debbda9104719d8c0e2ceab84682208428c1de0
Leonardo200298 <leonardosanchez119@gmail.com>
2023-01-18 17:38:23 -0300

    feat(add some type of mvc): add peliculas controller

--- api-route.js
+++ api-route.js
@@ -1,12 +1,19 @@
 "use strict"
+
+/* correr nodemon con npm run dev */
+
+const peliculasController = require('./controllers/peliculasController.js')
 const express = require('express');
 const app = express();
 const port = 3020;
 
 //tabla de ruteo 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
+/* app.get('/peliculas', function (req, res) {
+    peliculasController.conseguirTodasLasPeliculas(req, res);
+}); */
+
+/* Implicitamente javascript envia los parametros req y res al controlador peliculas */
+app.get('/peliculas',peliculasController.conseguirTodasLasPeliculas);
 
 //se queda escuchando el puerto
 app.listen(port, () => {
---
+++ controllers/peliculasController.js
@@ -0,0 +1,6 @@
+"use strict";
+
+exports.conseguirTodasLasPeliculas= (req, res)=>{
+    res.send("Hola soy la ruta de todas las pelis")
+}
+
